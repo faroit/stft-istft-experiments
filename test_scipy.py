f53037b214e58692b4e21f21c03bf2e269ad3881
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-03-04 22:22:34 +0100

    spaces

--- test_scipy.py
+++ test_scipy.py
@@ -7,13 +7,22 @@ import scipy.signal
 
 def stft(x, n_fft=2048, n_hopsize=1024):
     f, t, X = scipy.signal.stft(
-        x, nperseg=n_fft, noverlap=n_fft - n_hopsize, padded=True)
+        x, 
+        nperseg=n_fft, 
+        noverlap=n_fft - n_hopsize, 
+        padded=True
+    )
     return X * n_hopsize
 
 
 def istft(X, rate=44100, n_fft=2048, n_hopsize=1024):
     t, audio = scipy.signal.istft(
-        X / n_hopsize, rate, nperseg=n_fft, noverlap=n_fft - n_hopsize, boundary=True)
+        X / n_hopsize, 
+        rate, 
+        nperseg=n_fft, 
+        noverlap=n_fft - n_hopsize, 
+        boundary=True
+    )
     return audio
 
 
